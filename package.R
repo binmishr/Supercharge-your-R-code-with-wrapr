4ab0528679a45e6707fcce86d011b82d536359a3
BINAYAKA MISHRA <mishra.binayaka.18005@gmail.com>
2022-01-13 11:59:59 +0530

    Update package.R

--- package.R
+++ package.R
@@ -2,7 +2,6 @@
 #'
 #' Supplies insertDotPipeAdin.
 #'
-#' This package (\url{https://github.com/WinVector/addinexamplesWV}) is a fork of \url{https://github.com/rstudio/addinexamples}.
 #'
 #' @name addinexamplesWV
 #' @docType package
