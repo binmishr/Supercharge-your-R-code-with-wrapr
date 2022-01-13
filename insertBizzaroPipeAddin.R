24662c9382191077e3357b425ccb93513680cfea
BINAYAKA MISHRA <mishra.binayaka.18005@gmail.com>
2022-01-13 11:56:23 +0530

    Update insertBizzaroPipeAddin.R

--- insertBizzaroPipeAddin.R
+++ insertBizzaroPipeAddin.R
@@ -1,6 +1,5 @@
 #' Insert ->.; .
 #'
-#' Call this function as an addin to insert \code{->.;} (Bizarro Pipe, \url{http://www.win-vector.com/blog/2016/12/magrittrs-doppelganger/}) at the cursor position.
 #'
 #' @export
 insertBizzaroPipeAddin <- function() {
